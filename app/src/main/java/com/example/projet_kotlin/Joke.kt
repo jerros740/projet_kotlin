d44b1ece26cb2b19f7df1c66ad759445cd671df5
jerros740 <jeremy.ros@edu.esiee.fr>
2022-03-27 14:30:29 +0200

    Joke est désormais serializable et est une dataclass

--- app/src/main/java/com/example/projet_kotlin/Joke.kt
+++ app/src/main/java/com/example/projet_kotlin/Joke.kt
@@ -1,70 +1,12 @@
 package com.example.projet_kotlin
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import org.json.JSONObject
-import java.net.HttpURLConnection
-import java.net.URL
+import kotlinx.serialization.*
 
-@RequiresApi(Build.VERSION_CODES.N)
-class Joke(name: String)
-{
-    private val url = "https://api.chucknorris.io/jokes/random"
-    private var jsonJoke = JSONObject()
-    private var value = ""
-    init
-    {
-        value = name
+@Serializable
+data class Joke(
+    val categories: List<String>, @SerialName(value ="created_at") val createdAt: String, @SerialName(value ="icon_url") val iconUrl: String, val id:String, @SerialName(value ="updated_at") val updatedAt: String, val url:String,
+    val value: String)
 
-    }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun init()
-    {
-        val thread = Thread {
-            try {
-                val url = URL(url)
 
-                with(url.openConnection() as HttpURLConnection)
-                {
-                    requestMethod = "GET"
-                    var str = ""
-                    inputStream.bufferedReader().use {
-                        it.lines().forEach { line ->
-                            println(line)
-                            str += line
-                        }
-                    }
-                    // Conversion de la string reçu en JSON
-                    jsonJoke = JSONObject(str)
 
-                }
-            }
-            catch (e: Exception)
-            {
-                e.printStackTrace()
-            }
-        }
-        thread.start()
-    }
-
-    fun icon(): Any
-    {
-        return jsonJoke.get("icon_url")
-    }
-
-    fun url(): Any
-    {
-        return jsonJoke.get("url")
-    }
-
-    fun value(): Any
-    {
-        return jsonJoke.get("value")
-    }
-
-    fun name(): String
-    {
-        return value
-    }
-}
