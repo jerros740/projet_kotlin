05992d397790622f915bc28760e0807f738e9f80
jerros740 <jeremy.ros@edu.esiee.fr>
2022-05-09 12:05:09 +0200

    added : data class model

--- app/src/main/java/com/example/projet_kotlin/JokeView.kt
+++ app/src/main/java/com/example/projet_kotlin/JokeView.kt
@@ -2,7 +2,10 @@ package com.example.projet_kotlin
 
 import android.R
 import android.content.Context
+import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import org.w3c.dom.Text
 
 
 class JokeView @JvmOverloads constructor(context: Context) : ConstraintLayout(context) {
@@ -10,4 +13,5 @@ class JokeView @JvmOverloads constructor(context: Context) : ConstraintLayout(co
         inflate(getContext(),com.example.projet_kotlin.R.layout.joke_layout, this)
     }
 
+    data class Model(val btnStar : Button, val btnShare : Button, val txtJoke : TextView)
 }
