2d38f186ba7f988cd32b4351d407944ea55d889f
jerros740 <jeremy.ros@edu.esiee.fr>
2022-05-11 12:36:42 +0200

    added : star icon change (on/off) when clicked + new function to remove an item

--- app/src/main/java/com/example/projet_kotlin/JokeAdapter.kt
+++ app/src/main/java/com/example/projet_kotlin/JokeAdapter.kt
@@ -4,9 +4,8 @@ import android.content.ContentValues.TAG
 import android.content.Context
 import android.os.Build
 import android.util.Log
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
@@ -23,21 +22,29 @@ class JokeAdapter(jokes :List<Joke>, context: Context) : RecyclerView.Adapter<Jo
         this.context = context
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    class ViewHolder(view: JokeView) : RecyclerView.ViewHolder(view)
     {
-        val textView: TextView = view.findViewById(R.id.textView)
+        val textView: TextView = view.findViewById(R.id.txtViewJoke)
 
         init {
-            val btnShare: Button = view.findViewById(R.id.btnShare)
-            val btnStar: Button = view.findViewById(R.id.btnStar)
-            btnShare.setOnClickListener(View.OnClickListener { view ->
+            val btnShare: ImageButton = view.findViewById(R.id.btnShare)
+            val btnStar: ImageButton = view.findViewById(R.id.btnStar)
+            var on: Boolean = false
+            btnShare.setOnClickListener { view ->
                 Log.d(TAG, "Share")
-            })
-            btnStar.setOnClickListener(View.OnClickListener { view ->
+                Log.d(TAG, textView.text.toString())
+            }
+            btnStar.setOnClickListener { view ->
                 Log.d(TAG, "Star")
-            })
+                on = if(!on) {
+                    btnStar.setImageResource(android.R.drawable.btn_star_big_on)
+                    true
+                } else {
+                    btnStar.setImageResource(android.R.drawable.btn_star_big_off)
+                    false
+                }
+            }
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -63,5 +70,16 @@ class JokeAdapter(jokes :List<Joke>, context: Context) : RecyclerView.Adapter<Jo
     {
         listOfJoke.add(joke)
         this.notifyItemInserted(getItemCount() - 1);
+    }
+
+    /**
+     * @desc Remove item to the recycler view
+     * @param position - Remove the joke on a particular position
+     */
+    fun removeItem(position : Int)
+    {
+        listOfJoke.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, listOfJoke.size)
     }
 }
