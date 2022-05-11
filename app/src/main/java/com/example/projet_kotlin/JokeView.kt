3593edfc5563cacfb0bb0d30d10869dfcf4425fe
jerros740 <jeremy.ros@edu.esiee.fr>
2022-05-11 12:38:20 +0200

    changes : minor variable changes + default parameter

--- app/src/main/java/com/example/projet_kotlin/JokeView.kt
+++ app/src/main/java/com/example/projet_kotlin/JokeView.kt
@@ -1,17 +1,38 @@
 package com.example.projet_kotlin
 
-import android.R
 import android.content.Context
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import org.w3c.dom.Text
 
 
-class JokeView @JvmOverloads constructor(context: Context) : ConstraintLayout(context) {
+class JokeView constructor(context: Context) : ConstraintLayout(context) {
+
     init {
-        inflate(getContext(),com.example.projet_kotlin.R.layout.joke_layout, this)
+        inflate(getContext(), R.layout.joke_layout, this)
+
     }
 
-    data class Model(val btnStar : Button, val btnShare : Button, val txtJoke : TextView)
+    data class Model(val txtJoke: String = "", val imgBtnShare: Int = -1, val imgBtnStar: Int = -1)
+
+    /*
+    fun setupView(model : Model)
+    {
+        val txtViewJoke:TextView = findViewById(R.id.txtViewJoke)
+        val btnShare: ImageButton = findViewById(R.id.btnShare)
+        val btnStar: ImageButton = findViewById(R.id.btnStar)
+
+        var txtJoke = model.txtJoke
+        var drawableShare = model.imgBtnShare
+        var drawableStar = model.imgBtnStar
+
+        if(txtJoke != "")
+            txtViewJoke.setText(txtJoke)
+        if(drawableShare != -1)
+            btnShare.setImageResource(drawableShare)
+        if(drawableStar != -1)
+            btnStar.setImageResource(drawableStar)
+    }*/
 }
